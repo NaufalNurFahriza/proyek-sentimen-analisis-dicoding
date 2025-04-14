f315223da0169324f110e466ca70c6157e83606d
Naufalnurfahriza <123190024@student.upnyk.ac.id>
2025-04-14 20:22:52 +0700

    change to minecraft game from quora

--- data_scraping.py
+++ data_scraping.py
@@ -2,9 +2,9 @@ from google_play_scraper import reviews_all, Sort
 import pandas as pd
 
 def scrape_quora_reviews():
-    # Scrape ulasan dari aplikasi Quora di Google Play Store
+    # Scrape ulasan dari game Minecraft di Google Play Store
     reviews = reviews_all(
-        'com.quora.android',  # ID aplikasi Quora
+        'com.mojang.minecraftpe',  # ID aplikasi Quora
         lang='id',            # Bahasa ulasan
         country='id',         # Negara
         sort=Sort.MOST_RELEVANT,  # Urutan ulasan
@@ -14,10 +14,10 @@ def scrape_quora_reviews():
 
     # Simpan hasil ke CSV
     df = pd.DataFrame(reviews)
-    df.to_csv('Quora_reviews.csv', index=False, encoding='utf-8')
+    df.to_csv('Minecraft_reviews.csv', index=False, encoding='utf-8')
     
     # Menampilkan jumlah ulasan yang diambil
-    print(f"Scraped {len(df)} Quora reviews and saved to Quora_reviews.csv")
+    print(f"Scraped {len(df)} Minecraft reviews and saved to Minecraft_reviews.csv")
 
 if __name__ == "__main__":
     scrape_quora_reviews()
